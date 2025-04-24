8aa78f7464f1e05e85a3499ce7c68254c9ce0f16
Hritik Gautam <85284353+sujeet-12@users.noreply.github.com>
2025-04-24 09:35:18 +0530

    Update XO.ts

--- XO.ts
+++ XO.ts
@@ -1,1 +1,13 @@
+// main.ts
+const message = document.getElementById("message");
 
+if (message) {
+  window.addEventListener("scroll", () => {
+    const y = window.scrollY;
+    if (y > 100) {
+      message.textContent = "You're scrolling down!";
+    } else {
+      message.textContent = "Scroll down to see the fixed navbar.";
+    }
+  });
+}
